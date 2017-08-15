4c86be7c43ace1aa25da9539924fbd08690bec23
Palmer D'Orazio <palmer.dorazio@gmail.com>
2017-08-15 13:56:35 -0400

    it works

--- app.js
+++ app.js
@@ -18,13 +18,40 @@ $(document).ready(function() {
         }
 
         // split word into array
+        var array1 = word1.split("");
+        var array2 = word2.split("");
+
         // sort and rearrange array
+        array1.sort();
+        array2.sort();
 
         // compare arrays
 
+        if (arraysEqual(array1, array2)){
+            $('body').append('<p>' + word1 + ' ' + word2 + ' are anagrams. </p>')
+        }else{
+            $('body').append("<p> not anagrams </p>");
+        }
 
-        $('body').append('<p>' + word1 + ' ' + word2 + '</p>')
+        // $('body').append('<p>' + word1 + ' ' + word2 + '</p>')
 
     });
 
 });
+
+// https://stackoverflow.com/questions/3115982/how-to-check-if-two-arrays-are-equal-with-javascript
+
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length != b.length) return false;
+
+    // arrays are already sorted
+
+    for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+    }
+    return true;
+}
+
+//TODO: are we checking for anagram phrases?
