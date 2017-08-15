229b5500bc135e0d97d499134f8c0e3cfe54ac4b
Palmer D'Orazio <palmer.dorazio@gmail.com>
2017-08-15 13:48:53 -0400

    length check

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ $(document).ready(function() {
     var checkBtn = $('#check');
 
     checkBtn.click(function(){
-        $('body').append('<p>testing</p>')
+        // $('body').append('<p>testing</p>')
+
+        var word1 = $('#word1').val();
+        var word2 = $('#word2').val();
+
+        // check if length is equal
+        if (word1.length != word2.length){
+            $('body').append("not anagrams");
+            return;
+        }
+
+        // split word into array
+        // sort and rearrange array
+
+        // compare arrays
+
+
+        $('body').append('<p>' + word1 + ' ' + word2 + '</p>')
+
     });
 
 });
