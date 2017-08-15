6ef58625d195b9ea3e6d5b1361310c780b1255ff
Palmer D'Orazio <palmer.dorazio@gmail.com>
2017-08-14 22:44:05 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by palmer on 8/14/17.
+ */
+$(document).ready(function() {
+
+    var checkBtn = $('#check');
+
+    checkBtn.click(function(){
+        $('body').append('<p>testing</p>')
+    });
+
+});
