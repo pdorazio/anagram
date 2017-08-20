3d831653acb5640670f9c71c5ecb5da2c365d9de
Palmer D'Orazio <palmer.dorazio@gmail.com>
2017-08-19 21:33:36 -0400

    some cleanup

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 /**
  * Created by palmer on 8/14/17.
  */
+
 $(document).ready(function() {
 
     var checkBtn = $('#btn-check');
 
     checkBtn.click(function(){
-        // $('body').append('<p>testing</p>')
-        console.log("CLICK");
 
+        // get input strings
         var word1 = $('#word1').val();
         var word2 = $('#word2').val();
 
-        // check if length is equal
-        // if (word1.length != word2.length){
-        //     $('body').append("not anagrams");
-        //     return;
-        // }
+        // strip whitespace to detect anagram phrases
+        word1.replace(/ /g, "");
+        word2.replace(/ /g, "");
 
+        // return if empty
         if (word1.length < 1 || word2.length < 1){
             return;
         }
-
-        // TODO: should i check that words are, like... words?
 
         // split word into array
         var array1 = word1.split("");
@@ -34,20 +31,15 @@ $(document).ready(function() {
         array2.sort();
 
         // compare arrays
-
         if (arraysEqual(array1, array2)){
-            //$('body').append('<p>' + word1 + ' ' + word2 + ' are anagrams. </p>')
             $('#results').prepend('<div class="result result-positive"><p class="result-text">' + '<span class="positive-text">' + word1 + '</span> and <span class="positive-text">' + word2 +  '</span> are anagrams. </p></div>')
         }else{
-            //$('body').append("<p> not anagrams </p>");
-            //$('#results').prepend('<div class="result result-negative"><p class="result-text">' + word1 + ' and ' + word2 +  ' are not anagrams. </p></div>')
             $('#results').prepend('<div class="result result-negative"><p class="result-text">' + '<span class="negative-text">' + word1 + '</span> and <span class="negative-text">' + word2 +  '</span> are not anagrams. </p></div>')
         }
 
+        // reset input fields
         $('#word1').val("");
         $('#word2').val("");
-
-        // $('body').append('<p>' + word1 + ' ' + word2 + '</p>')
 
     });
 
@@ -60,14 +52,8 @@ function arraysEqual(a, b) {
     if (a == null || b == null) return false;
     if (a.length != b.length) return false;
 
-    // arrays are already sorted
-
     for (var i = 0; i < a.length; ++i) {
         if (a[i] !== b[i]) return false;
     }
     return true;
 }
-
-// TODO: are we checking for anagram phrases?
-
-// TODO: add spans so i can style word1 and word2 in their result row.
